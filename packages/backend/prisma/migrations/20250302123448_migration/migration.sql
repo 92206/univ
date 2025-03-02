41a4a9d57e558e75eeb6c1577e80faecdc1ee2ec
92206 <09126793s@gmail.com>
2025-03-02 13:24:26 +0100

    updates to the chema

---
+++ packages/backend/prisma/migrations/20250302123448_migration/migration.sql
@@ -0,0 +1,394 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "linkedinAccount" TEXT,
+    "dateOfBirth" TIMESTAMP(3) NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "displayName" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "permission" TEXT[],
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ResearchPaper" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "doi" TEXT NOT NULL,
+    "publicationDate" TIMESTAMP(3) NOT NULL,
+    "pdfUrl" TEXT NOT NULL,
+    "nbCitation" INTEGER NOT NULL,
+    "abstract" TEXT,
+
+    CONSTRAINT "ResearchPaper_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Keyword" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Keyword_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "InstitutionWorkerProfile" (
+    "id" SERIAL NOT NULL,
+    "position" TEXT NOT NULL,
+    "teachingPosition" BOOLEAN NOT NULL,
+    "researchPosition" BOOLEAN NOT NULL,
+    "administrationPosition" BOOLEAN NOT NULL,
+    "department" TEXT NOT NULL,
+    "otherInstitution" TEXT,
+    "isInstitutionProfileManager" BOOLEAN NOT NULL DEFAULT false,
+    "isPrimary" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    "institutionId" INTEGER NOT NULL,
+
+    CONSTRAINT "InstitutionWorkerProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "StudentInstituionAffiliation" (
+    "id" SERIAL NOT NULL,
+    "enrolment_year" INTEGER NOT NULL,
+    "program" TEXT,
+    "institutionId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "bacScore" DOUBLE PRECISION,
+
+    CONSTRAINT "StudentInstituionAffiliation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Institution" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "website" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
+    "accreditation" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "parentInstitutionId" INTEGER,
+    "institutionKindId" INTEGER NOT NULL,
+
+    CONSTRAINT "Institution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "InstitutionKind" (
+    "id" SERIAL NOT NULL,
+    "type" TEXT NOT NULL,
+    "isPublic" BOOLEAN NOT NULL,
+
+    CONSTRAINT "InstitutionKind_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "location" TEXT NOT NULL,
+    "coordinate" TEXT NOT NULL,
+    "online" BOOLEAN NOT NULL,
+    "organizer" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "institutionId" INTEGER NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ResearchOffer" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "qualification" TEXT NOT NULL,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "contact" TEXT NOT NULL,
+    "institutionId" INTEGER,
+
+    CONSTRAINT "ResearchOffer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ResearchLab" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
+    "institutionId" INTEGER,
+
+    CONSTRAINT "ResearchLab_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ResearchTeam" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
+    "institutionId" INTEGER,
+    "researchLabId" INTEGER,
+
+    CONSTRAINT "ResearchTeam_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Curriculum" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "degree" TEXT NOT NULL,
+    "admissionRequirement" TEXT NOT NULL,
+    "applicationProcedure" TEXT NOT NULL,
+    "tuition" INTEGER NOT NULL,
+    "institutionId" INTEGER NOT NULL,
+    "curriculumKindId" INTEGER NOT NULL,
+
+    CONSTRAINT "Curriculum_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Field" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Field_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CurriculumKind" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "isDegree" BOOLEAN NOT NULL,
+
+    CONSTRAINT "CurriculumKind_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Term" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "curriculumId" INTEGER NOT NULL,
+
+    CONSTRAINT "Term_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" SERIAL NOT NULL,
+    "courseDate" TIMESTAMP(3) NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "credits" INTEGER NOT NULL,
+    "preReqs" TEXT NOT NULL,
+    "termId" INTEGER NOT NULL,
+    "institutionId" INTEGER NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Funding" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "contactEmail" TEXT NOT NULL,
+    "contactLink" TEXT NOT NULL,
+    "institutionId" INTEGER NOT NULL,
+
+    CONSTRAINT "Funding_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_Author" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_Author_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_PaperKeywords" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_PaperKeywords_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_BlogTags" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_BlogTags_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_EventTags" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_EventTags_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_ResearchTeamToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_ResearchTeamToUser_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_CurriculumToField" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_CurriculumToField_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_Author_B_index" ON "_Author"("B");
+
+-- CreateIndex
+CREATE INDEX "_PaperKeywords_B_index" ON "_PaperKeywords"("B");
+
+-- CreateIndex
+CREATE INDEX "_BlogTags_B_index" ON "_BlogTags"("B");
+
+-- CreateIndex
+CREATE INDEX "_EventTags_B_index" ON "_EventTags"("B");
+
+-- CreateIndex
+CREATE INDEX "_ResearchTeamToUser_B_index" ON "_ResearchTeamToUser"("B");
+
+-- CreateIndex
+CREATE INDEX "_CurriculumToField_B_index" ON "_CurriculumToField"("B");
+
+-- AddForeignKey
+ALTER TABLE "InstitutionWorkerProfile" ADD CONSTRAINT "InstitutionWorkerProfile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "InstitutionWorkerProfile" ADD CONSTRAINT "InstitutionWorkerProfile_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentInstituionAffiliation" ADD CONSTRAINT "StudentInstituionAffiliation_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentInstituionAffiliation" ADD CONSTRAINT "StudentInstituionAffiliation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Institution" ADD CONSTRAINT "Institution_parentInstitutionId_fkey" FOREIGN KEY ("parentInstitutionId") REFERENCES "Institution"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Institution" ADD CONSTRAINT "Institution_institutionKindId_fkey" FOREIGN KEY ("institutionKindId") REFERENCES "InstitutionKind"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResearchOffer" ADD CONSTRAINT "ResearchOffer_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResearchLab" ADD CONSTRAINT "ResearchLab_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResearchTeam" ADD CONSTRAINT "ResearchTeam_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ResearchTeam" ADD CONSTRAINT "ResearchTeam_researchLabId_fkey" FOREIGN KEY ("researchLabId") REFERENCES "ResearchLab"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Curriculum" ADD CONSTRAINT "Curriculum_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Curriculum" ADD CONSTRAINT "Curriculum_curriculumKindId_fkey" FOREIGN KEY ("curriculumKindId") REFERENCES "CurriculumKind"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Term" ADD CONSTRAINT "Term_curriculumId_fkey" FOREIGN KEY ("curriculumId") REFERENCES "Curriculum"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Course" ADD CONSTRAINT "Course_termId_fkey" FOREIGN KEY ("termId") REFERENCES "Term"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Course" ADD CONSTRAINT "Course_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Funding" ADD CONSTRAINT "Funding_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Author" ADD CONSTRAINT "_Author_A_fkey" FOREIGN KEY ("A") REFERENCES "ResearchPaper"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Author" ADD CONSTRAINT "_Author_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PaperKeywords" ADD CONSTRAINT "_PaperKeywords_A_fkey" FOREIGN KEY ("A") REFERENCES "Keyword"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PaperKeywords" ADD CONSTRAINT "_PaperKeywords_B_fkey" FOREIGN KEY ("B") REFERENCES "ResearchPaper"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BlogTags" ADD CONSTRAINT "_BlogTags_A_fkey" FOREIGN KEY ("A") REFERENCES "Blog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BlogTags" ADD CONSTRAINT "_BlogTags_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EventTags" ADD CONSTRAINT "_EventTags_A_fkey" FOREIGN KEY ("A") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EventTags" ADD CONSTRAINT "_EventTags_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ResearchTeamToUser" ADD CONSTRAINT "_ResearchTeamToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "ResearchTeam"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ResearchTeamToUser" ADD CONSTRAINT "_ResearchTeamToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CurriculumToField" ADD CONSTRAINT "_CurriculumToField_A_fkey" FOREIGN KEY ("A") REFERENCES "Curriculum"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CurriculumToField" ADD CONSTRAINT "_CurriculumToField_B_fkey" FOREIGN KEY ("B") REFERENCES "Field"("id") ON DELETE CASCADE ON UPDATE CASCADE;
